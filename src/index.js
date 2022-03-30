aad0625a5c01bd6eb900cf5d34bdd90b815015de
VictorUlian <yctor.ulian@gmail.com>
2022-03-29 23:53:19 -0300

    Arquivos iniciais do react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './assets/styles/index.css';
+import './assets/styles/reset.css';
+import App from './compounds/App.js';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+ReactDOM.render(<App />, document.querySelector(".root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
